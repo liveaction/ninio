dab62b1843e127d76281e38c2ec5840d37f11c39
Simon Daudin <sdaudin@liveaction.com>
2022-11-17 16:47:38 +0100

    test(util): make MemoryCacheTest not time dependant

--- ninio-util/src/test/java/com/davfx/ninio/util/MemoryCacheTest.java
+++ ninio-util/src/test/java/com/davfx/ninio/util/MemoryCacheTest.java
@@ -7,24 +7,26 @@ public class MemoryCacheTest {
 	@Test
 	public void testExpireAfterWrite() throws Exception {
 		MemoryCache<String, String> cache = MemoryCache.<String, String> builder().expireAfterWrite(0.1d).build();
+		cache.nowSupplier = () -> 0;
 		cache.put("k", "v");
 		Assertions.assertThat(cache.get("k")).isEqualTo("v");
-		Thread.sleep(50);
+		cache.nowSupplier = () -> 0.05d;
 		Assertions.assertThat(cache.get("k")).isEqualTo("v");
-		Thread.sleep(70);
+		cache.nowSupplier = () -> 0.12d;
 		Assertions.assertThat(cache.get("k")).isNull();
 	}
 
 	@Test
 	public void testExpireAfterAccess() throws Exception {
 		MemoryCache<String, String> cache = MemoryCache.<String, String> builder().expireAfterAccess(0.1d).build();
+		cache.nowSupplier = () -> 0;
 		cache.put("k", "v");
 		Assertions.assertThat(cache.get("k")).isEqualTo("v");
-		Thread.sleep(50);
+		cache.nowSupplier = () -> 0.05d;
 		Assertions.assertThat(cache.get("k")).isEqualTo("v");
-		Thread.sleep(70);
+		cache.nowSupplier = () -> 0.12d;
 		Assertions.assertThat(cache.get("k")).isEqualTo("v");
-		Thread.sleep(150);
+		cache.nowSupplier = () -> 0.27d;
 		Assertions.assertThat(cache.get("k")).isNull();
 	}
 }
